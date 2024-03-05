4b0ad3236d89748c80f541da473a692a973f9851
Brock Britton <brockbritton@outlook.com>
2024-03-04 21:41:32 -0800

    error handling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios';
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const LOCATION_IQ_API_KEY = import.meta.env.VITE_LOC_IQ_API_KEY;
 
@@ -12,40 +13,43 @@ function App() {
     let value = event.target.value;
     setCity(value);
   }
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (city.length === 0) {
-      alert("Please enter a city name.")
-    } else {
-      getLocation(city);
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getLocation(city);
+  
     
   }
   const getLocation = async (cityName) => {
-    let response = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${LOCATION_IQ_API_KEY}&q=${cityName}&format=json`);
-    console.log(response)
-    setResponseData(response.data[0]);
+    try {
+      let response = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${LOCATION_IQ_API_KEY}&q=${cityName}&format=json`);
+      console.log(response)
+      setResponseData(response.data[0]);
+    } catch(error) {
+      console.log(error)
+      alert(`${error}. Please try again.`)
+    }
+    
   }
 
   return (
     <>
       <header>
-        <h1>Geolocation</h1>
-        <form>
-          <input type="text" placeholder="Enter a city name" onChange={handleInput} />
+        <h1>City Geolocator !!</h1>
+        <form id='city-input-form'>
+          <input id='city-input' type="text" placeholder="Enter a city name" onChange={handleInput} />
           <button onClick={handleSubmit}>
             Explore!
           </button>
         </form>
       </header>
-      <div className="card">
+      <div className="display-card">
         {responseData.display_name
         ? <ol>
-            <p>{responseData.display_name}</p>
+            <h3>{responseData.display_name}</h3>
             <p>{`Latitude: ${responseData.lat}, Longitude: ${responseData.lon}`}</p>
             <img src={`https://maps.locationiq.com/v3/staticmap?key=${LOCATION_IQ_API_KEY}&center=${responseData.lat},${responseData.lon}&zoom=12`}/>
           </ol>
-        : <p>Please Click the Explore</p>
+        : <p> Please Click the Explore Button </p>
         }
       </div>
     </>
